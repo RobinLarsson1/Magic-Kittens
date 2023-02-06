421489a12fd3782d18eed6a7f8c942c9c262cb44
RobinLarsson1 <Robin.Larsson01@gmail.com>
2023-02-06 17:41:36 +0100

    update

--- script.js
+++ script.js
@@ -63,6 +63,8 @@ let wrongGuesses = 0
 
 let disabledKeyList = ['Enter', 'Control', 'ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp', ' ', 'Meta', 'Alt', 'AltGraph', 'ContextMenu', 'Home', 'End', 'PageDown', 'PageUp', 'Shift', 'Delete', 'Backspace', 'Insert', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', '§', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'CapsLock', 'Tab']
 
+
+
 document.onkeydown = function(event) {
     console.log(event.key)
     let charArray = secretWord.split("");
