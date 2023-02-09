3cba228a919aef9d184bdda750d68245b4a36a1d
vectorell <vlindell@outlook.com>
2023-02-09 10:26:50 +0100

    continued work

--- script.js
+++ script.js
@@ -138,121 +138,121 @@ let p2name
 
 const LS_KEY = 'hangman-score'
 
-function publishStats(result) {
-    let currentResult = {
-        name: p1name,
-        word: secretWord,
-        tries: mistakes,
-        won: result
-    }
+// function publishStats(result) {
+//     let currentResult = {
+//         name: p1name,
+//         word: secretWord,
+//         tries: mistakes,
+//         won: result
+//     }
     
-    // steg 1: hämta data från localStorage
-    // ifsats kontrollerar om det inte finns någon data sen innan
-    let stringFromLocalStorage = localStorage.getItem(LS_KEY)
-    if (!stringFromLocalStorage) {
-        stringFromLocalStorage = '[]'
-    }
+//     // steg 1: hämta data från localStorage
+//     // ifsats kontrollerar om det inte finns någon data sen innan
+//     let stringFromLocalStorage = localStorage.getItem(LS_KEY)
+//     if (!stringFromLocalStorage) {
+//         stringFromLocalStorage = '[]'
+//     }
     
-    // omvandlar JSON-strängen till array med namn 'results'
-    let results = JSON.parse(stringFromLocalStorage)
+//     // omvandlar JSON-strängen till array med namn 'results'
+//     let results = JSON.parse(stringFromLocalStorage)
     
-    // pushar in senaste omgång till result-arrayen
-    results.push(currentResult)
+//     // pushar in senaste omgång till result-arrayen
+//     results.push(currentResult)
 
-    // mha annan funktion - renderar listan på scoreboard-sidan
-    renderStats(results)
+//     // mha annan funktion - renderar listan på scoreboard-sidan
+//     renderStats(results)
 
-    // lägger tillbaka arrayen till localStorage (görs om till JSON-string)
-    let stringToSave = JSON.stringify(results)
-    localStorage.setItem(LS_KEY, stringToSave)
-}
+//     // lägger tillbaka arrayen till localStorage (görs om till JSON-string)
+//     let stringToSave = JSON.stringify(results)
+//     localStorage.setItem(LS_KEY, stringToSave)
+// }
 
-function renderStats(results) {
-    let displayScoreContainer = document.querySelector('.container-display-score')
+// function renderStats(results) {
+//     let displayScoreContainer = document.querySelector('.container-display-score')
 
-        const removePlayerData = () => {
-            console.log(`removePlayerData`, results);
-            const playerNameInputValue = playerNameInput.value.toLowerCase()
-            // const elementName = element.name
-            const elementName = results.forEach(element => element.name.toLowerCase())
-                if(playerNameInputValue === elementName) {
-                    p.remove()
-                    const saveFilterResult = results.filter(result => result.name !== playerNameInputValue)
+//         const removePlayerData = () => {
+//             console.log(`removePlayerData`, results);
+//             const playerNameInputValue = playerNameInput.value.toLowerCase()
+//             // const elementName = element.name
+//             const elementName = results.forEach(element => element.name.toLowerCase())
+//                 if(playerNameInputValue === elementName) {
+//                     p.remove()
+//                     const saveFilterResult = results.filter(result => result.name !== playerNameInputValue)
                     
-                    let saveNewString = JSON.stringify(saveFilterResult)
-                    localStorage.setItem(LS_KEY, saveNewString)
-                }
-        }
+//                     let saveNewString = JSON.stringify(saveFilterResult)
+//                     localStorage.setItem(LS_KEY, saveNewString)
+//                 }
+//         }
 
-        // BUG 2: Om man trycker på en funktionknapp eller mellanslag så registerar spelet samma resultat igen. Men localstorage uppdateras inte vilket är bra.
-        // BUG 3: Man kan fortfarande spela spelet även om man har en modal uppe.
-        const playerNameInput = document.querySelector('#select-player-data-using-input')
-        playerNameInput.addEventListener('keydown', event => {
-            event.stopPropagation()
-            if (event.key == 'Enter') {
-                    removePlayerData()
-                    //playerNameInput.value = null
-            }
-        })
+//         // BUG 2: Om man trycker på en funktionknapp eller mellanslag så registerar spelet samma resultat igen. Men localstorage uppdateras inte vilket är bra.
+//         // BUG 3: Man kan fortfarande spela spelet även om man har en modal uppe.
+//         const playerNameInput = document.querySelector('#select-player-data-using-input')
+//         playerNameInput.addEventListener('keydown', event => {
+//             event.stopPropagation()
+//             if (event.key == 'Enter') {
+//                     removePlayerData()
+//                     //playerNameInput.value = null
+//             }
+//         })
 
-        const playerNameInputButton = document.querySelector('#select-player-data-using-input-button')
-        playerNameInputButton.addEventListener('click', () => {
-                removePlayerData()
-                //playerNameInput.value = null
-        })
+//         const playerNameInputButton = document.querySelector('#select-player-data-using-input-button')
+//         playerNameInputButton.addEventListener('click', () => {
+//                 removePlayerData()
+//                 //playerNameInput.value = null
+//         })
 
-    // Skapa de DOMelement som behövs 
-    results.forEach(element => {
-        let p = document.createElement('p')
-        p.className = 'player-result'
+//     // Skapa de DOMelement som behövs 
+//     results.forEach(element => {
+//         let p = document.createElement('p')
+//         p.className = 'player-result'
         
-        p.innerHTML = `Namn: ${element.name}, <br> Ord: ${element.word}, <br> Felgissningar: ${element.tries} <br> Vinst? ${element.won}`
+//         p.innerHTML = `Namn: ${element.name}, <br> Ord: ${element.word}, <br> Felgissningar: ${element.tries} <br> Vinst? ${element.won}`
 
-        displayScoreContainer.append(p)
+//         displayScoreContainer.append(p)
 
             
-        // Knappar
+//         // Knappar
 
-        // Det denna gör är att den kollar om elementet (win) har egenskapen true
-        const listAllWinsButton = document.querySelector('#list-only-wins-button')
-        listAllWinsButton.addEventListener('click', event => {
-            if(!element.won == true) {
-                // p.style.display = 'none'
-                p.classList.add('list-element-hidden')
-            } else {
-                // p.style.display = 'block'
-                p.classList.remove('list-element-hidden')
-            }
-        })
+//         // Det denna gör är att den kollar om elementet (win) har egenskapen true
+//         const listAllWinsButton = document.querySelector('#list-only-wins-button')
+//         listAllWinsButton.addEventListener('click', event => {
+//             if(!element.won == true) {
+//                 // p.style.display = 'none'
+//                 p.classList.add('list-element-hidden')
+//             } else {
+//                 // p.style.display = 'block'
+//                 p.classList.remove('list-element-hidden')
+//             }
+//         })
 
-        // Det denna gör är att den kollar om elementet (win) har egenskapen true
-        const listAllLossesButton = document.querySelector('#list-only-losses-button')
-        listAllLossesButton.addEventListener('click', event => {
-            if(!element.won == false) {
-                p.classList.add('list-element-hidden')
-            } else {
-                p.classList.remove('list-element-hidden')
-            }
-        })
+//         // Det denna gör är att den kollar om elementet (win) har egenskapen true
+//         const listAllLossesButton = document.querySelector('#list-only-losses-button')
+//         listAllLossesButton.addEventListener('click', event => {
+//             if(!element.won == false) {
+//                 p.classList.add('list-element-hidden')
+//             } else {
+//                 p.classList.remove('list-element-hidden')
+//             }
+//         })
 
-        // Funkar bara om man har tryckt på vinster eller förluster först
-        const listAllResultsButton = document.querySelector('#list-all-results-button')
-        listAllResultsButton.addEventListener('click', event => {
-            if(!element.won == false || !element.won == true) {
-                // p.style.display = 'block'
-                p.classList.remove('list-element-hidden')
-            }
-        })
+//         // Funkar bara om man har tryckt på vinster eller förluster först
+//         const listAllResultsButton = document.querySelector('#list-all-results-button')
+//         listAllResultsButton.addEventListener('click', event => {
+//             if(!element.won == false || !element.won == true) {
+//                 // p.style.display = 'block'
+//                 p.classList.remove('list-element-hidden')
+//             }
+//         })
 
-    });
-}
+//     });
+// }
 
 //
 
 
 
 function publishStats(result) {
-    let currentResultPVP = {
+    let currentResult = {
         name1: p1name,
         name2: p2name,
         word: secretWord,
@@ -273,7 +273,7 @@ function publishStats(result) {
     let resultsPVP = JSON.parse(stringFromLocalStorage)
     
     // pushar in senaste omgång till result-arrayen
-    resultsPVP.push(currentResultPVP)
+    resultsPVP.push(currentResult)
 
     // mha annan funktion - renderar listan på scoreboard-sidan
     renderStats(resultsPVP)
@@ -284,15 +284,15 @@ function publishStats(result) {
 }
 
 function renderStats(resultsPVP) {
-    let displayScoreContainer = document.querySelector('.container-display-score')
-    
-    // Skapa de DOMelement som behövs 
-    resultsPVP.forEach(element => {
-        let p = document.createElement('p')
-        p.className = 'player-result'
+        let displayScoreContainer = document.querySelector('.container-display-score')
         
-        p.innerHTML = `PVP <br> ${element.name1}, ordskapare, spelade mot ${element.name2} som gissade. <br> Ordet var ${element.word}, <br> Felgissningar: ${element.tries} <br> Vinst? ${element.won}`
-
-        displayScoreContainer.append(p)
-    });
-}
+        // Skapa de DOMelement som behövs 
+        resultsPVP.forEach(element => {
+            let p = document.createElement('p')
+            p.className = 'player-result'
+            
+            p.innerHTML = `PVP <br> ${element.name1}, ordskapare, spelade mot ${element.name2} som gissade. <br> Ordet var ${element.word}, <br> Felgissningar: ${element.tries} <br> Vinst? ${element.won}`
+    
+            displayScoreContainer.append(p)
+        });
+    }
